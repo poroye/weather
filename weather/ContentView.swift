52916253c067980f257d477ae4e004cc0a585c20
tanat <31177223+poroye@users.noreply.github.com>
2021-09-06 01:44:20 -0700

    update
    ui add

--- weather/ContentView.swift
+++ weather/ContentView.swift
@@ -7,10 +7,51 @@
 
 import SwiftUI
 
+struct weather:Hashable{
+    let date:String
+    let status:String
+}
+
 struct ContentView: View {
+    
+    let test = [weather(date:"2020-06-28", status: "Fog")   ,weather(date:"2020-06-29", status: "Thunderstorm") ,weather(date:"2020-06-30", status: "Broken clouds")    ,weather(date:"2020-07-01", status: "Light shower snow")    ,weather(date:"2020-07-02", status: "Extreme rain") ,weather(date:"2020-07-03", status: "Clear sky")    ,weather(date:"2020-07-04", status: "Scattered clouds") ,weather(date:"2020-07-05", status: "Shower rain")]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List{
+            ForEach(test,id:\.self){ day in
+                HStack{
+                    VStack(alignment: .leading){
+                        Text(day.date)
+                        Text(day.status)
+                    }
+                    Spacer()
+                    imageView(status: day.status)
+                }
+            }
+        }
+    }
+}
+
+struct imageView: View {
+    let status:String
+    var body: some View{
+        if status == "Fog"{
+            Image("50d")
+        }else if status == "Thunderstorm"{
+            Image("11d")
+        }else if status == "Broken clouds"{
+            Image("04d")
+        }else if status == "Light shower snow"{
+            Image("13d")
+        }else if status == "Extreme rain"{
+            Image("10d")
+        }else if status == "Clear sky"{
+            Image("01d")
+        }else if status == "Scattered clouds"{
+            Image("13d")
+        }else if status == "Shower rain"{
+            Image("09d")
+        }
     }
 }
 
