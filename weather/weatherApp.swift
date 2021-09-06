545d6e9d6e812316b86e288ebddba6c1e41be927
tanat <31177223+poroye@users.noreply.github.com>
2021-09-06 00:52:17 -0700

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by ธนัท แสงเพิ่ม on 6/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
